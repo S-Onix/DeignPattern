8fda446a147f319401ee1bbc9827e282122403a6
S-Onix <msyoo101@hansung.ac.kr>
2018-09-07 19:59:00 +0900

    Design Pattern repository update, singleTon pattern update

---
+++ patternPractice/src/Main.java
@@ -0,0 +1,7 @@
+import singleton.SingletonEx;
+
+public class Main {
+    public static void main(String [] args){
+        SingletonEx singleTon = new SingletonEx();
+    }
+}
---
+++ patternPractice/src/singleton/SingletonEx.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class SingletonEx {
+    private static SingletonEx singleton = new SingletonEx();
+
+    private SingletonEx(){
+
+    }
+
+    static SingletonEx getInstance(){
+        return singleton;
+    }
+}
